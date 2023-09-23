ee8a407f14ced888ba3766b41e8a68ac869d6064
xue-fei <awnuxcvbn@163.com>
2023-09-23 23:14:07 +0800

    1、udp发送mpu6050 json数据

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -11,7 +11,8 @@
 该教程为对物联网开发感兴趣的朋友所设计和制作。如需了解更多该教程的信息，请参考以下网页：
 http://www.taichi-maker.com/homepage/esp8266-nodemcu-iot/iot-c/esp8266-nodemcu-web-client/http-request/
 ***********************************************************************/
- 
+#include <Wire.h>
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
@@ -25,12 +26,19 @@ WiFiUDP Udp;//实例化WiFiUDP对象
 unsigned int localUdpPort = 16651;  // 自定义本地监听端口
 unsigned int remoteUdpPort = 16650;  // 自定义远程监听端口
 char incomingPacket[255];  // 保存Udp工具发过来的消息
-char  replyPacket[] = "Hi, this is esp8266\n";  //发送的消息,仅支持英文
-char  shootPacket[] = "Shoot\n";
+char replyPacket[2048];  //发送的消息,仅支持英文
  
+const int MPU6050_addr = 0x68;
+int16_t AccX, AccY, AccZ, Temp, GyroX, GyroY, GyroZ;
 
 void setup()
 {
+  Wire.begin();
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
   Serial.begin(9600);//打开串口
   Serial.println();
  
@@ -43,13 +51,17 @@ void setup()
   }
   Serial.println("连接成功");
  
-  if(Udp.begin(localUdpPort)){//启动Udp监听服务
+ //启动Udp监听服务
+  if(Udp.begin(localUdpPort))
+  {
     Serial.println("监听成功");
       
     //打印本地的ip地址，在UDP工具中会使用到
     //WiFi.localIP().toString().c_str()用于将获取的本地IP地址转化为字符串   
     Serial.printf("现在收听IP：%s, UDP端口：%d\n", WiFi.localIP().toString().c_str(), localUdpPort);
-  }else{
+  }
+  else
+  {
     Serial.println("监听失败");
   }
   pinMode(BTN_1, INPUT_PULLUP);//开关按钮为输入开启上拉电阻
@@ -57,15 +69,41 @@ void setup()
  
 void loop()
 {
+  Wire.beginTransmission(MPU6050_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_addr, 14, true);
+  AccX = Wire.read() << 8 | Wire.read();
+  AccY = Wire.read() << 8 | Wire.read();
+  AccZ = Wire.read() << 8 | Wire.read();
+  Temp = Wire.read() << 8 | Wire.read();
+  GyroX = Wire.read() << 8 | Wire.read();
+  GyroY = Wire.read() << 8 | Wire.read();
+  GyroZ = Wire.read() << 8 | Wire.read();
+ 
+  DynamicJsonDocument doc(2048);
+  // 创建json根节点对象
+  JsonObject obj  = doc.to<JsonObject>();
+  obj ["AccX"] = AccX;
+  obj ["AccY"] = AccY;
+  obj ["AccZ"] = AccZ;
+  obj ["Temp"] = Temp;
+  obj ["GyroX"] = GyroX;
+  obj ["GyroY"] = GyroY;
+  obj ["GyroZ"] = GyroZ;  
   //向udp工具发送消息
-  Udp.beginPacket(Udp.remoteIP(), remoteUdpPort);//配置远端ip地址和端口
-  Udp.print(replyPacket);//把数据写入发送缓冲区
-  Serial.println("BTN_1:"+digitalRead(BTN_1));
+  Udp.beginPacket(Udp.remoteIP(), remoteUdpPort);//配置远端ip地址和端口 
   if (digitalRead(BTN_1) == 0)
   {
-    Udp.print(shootPacket);//把数据写入发送缓冲区
+      obj ["Shoot"] = 1; 
   }
-  Udp.endPacket();//发送数据
-  Serial.println("UDP数据发送成功");
-  delay(33);//延时33毫秒
+  else
+  {
+      obj ["Shoot"] = 0; 
+  }
+  String output;
+  serializeJson(doc, replyPacket);    
+  Udp.print(replyPacket);//把数据写入发送缓冲区
+  Udp.endPacket();//发送数据 
+  delay(16);//延时33毫秒
 }
